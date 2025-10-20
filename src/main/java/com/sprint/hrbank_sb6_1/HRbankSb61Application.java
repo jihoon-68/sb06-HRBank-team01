294fef51de1e449c55af97f4f8d55cacdf33f315
taehun88 <taehun8235@gmail.com>
2025-10-20 17:29:14 +0900

    feat:project init

---
+++ src/main/java/com/sprint/hrbank_sb6_1/HRbankSb61Application.java
@@ -0,0 +1,13 @@
+package com.sprint.hrbank_sb6_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HRbankSb61Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HRbankSb61Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sprint/hrbank_sb6_1/HRbankSb61ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.hrbank_sb6_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HRbankSb61ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
