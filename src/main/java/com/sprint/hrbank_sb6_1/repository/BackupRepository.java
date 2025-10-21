706dc31ddac9474e1d3a67cea73263cf1747e88d
jihoon-68 <pkrwlgns64@gmail.com>
2025-10-21 13:42:34 +0900

    feat: BackupRepository 추가

---
+++ src/main/java/com/sprint/hrbank_sb6_1/repository/BackupRepository.java
@@ -0,0 +1,9 @@
+package com.sprint.hrbank_sb6_1.repository;
+
+import com.sprint.hrbank_sb6_1.domain.Backup;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BackupRepository extends JpaRepository<Backup, Long> {
+}
