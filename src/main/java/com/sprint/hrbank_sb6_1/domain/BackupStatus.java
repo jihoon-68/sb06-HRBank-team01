e9a83fa7d4d2983567aaa886ba6bf6f35a81875c
taehun88 <taehun8235@gmail.com>
2025-10-21 09:37:51 +0900

    feat:create entity

---
+++ src/main/java/com/sprint/hrbank_sb6_1/domain/BackupStatus.java
@@ -0,0 +1,6 @@
+package com.sprint.hrbank_sb6_1.domain;
+
+public enum BackupStatus {
+    DONE,
+    NOT_DONE
+}
---
+++ src/main/java/com/sprint/hrbank_sb6_1/domain/ChangeLog.java
@@ -0,0 +1,46 @@
+package com.sprint.hrbank_sb6_1.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class ChangeLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private ChangeLogStatus status;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column
+    private String memo;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private Timestamp at;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+
+}
