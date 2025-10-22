967139e6ab85fd9c9a43fae2ccac608f481a3e4d
jionu102 <128346722+jionu102@users.noreply.github.com>
2025-10-22 10:55:28 +0900

    [이진우] feat: 직원 등록 및 수정 기능 추가 (#28)
    * feat: 직원 등록 및 수정 부분 완료(의존성 대기)
    
    * feat: 직원 수정 및 등록 기능 추가 완료
    
    * [이진우] refactor: employee ent에 update메서드 생성 및 서비스 로직 setter -> update

--- src/main/java/com/sprint/hrbank_sb6_1/domain/ChangeLog.java
+++ src/main/java/com/sprint/hrbank_sb6_1/domain/ChangeLog.java
@@ -1,18 +1,11 @@
 package com.sprint.hrbank_sb6_1.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import lombok.*;
 
 @Getter
 @Setter
@@ -26,6 +19,7 @@ public class ChangeLog {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private ChangeLogStatus status;
 
