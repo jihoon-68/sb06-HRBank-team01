bd99ff318b37cdee1af66c446e62b5f3795bdd29
jihoon-68 <pkrwlgns64@gmail.com>
2025-10-23 23:46:11 +0900

    feat: 백업 실패시 로그파일 생성 및 로그 파일정보 DB에 저장

--- src/main/java/com/sprint/hrbank_sb6_1/service/basic/BasicBackupIoService.java
+++ src/main/java/com/sprint/hrbank_sb6_1/service/basic/BasicBackupIoService.java
@@ -10,7 +10,6 @@ import com.sprint.hrbank_sb6_1.repository.EmployeeRepository;
 import com.sprint.hrbank_sb6_1.repository.FileRepository;
 import com.sprint.hrbank_sb6_1.service.BackupIoService;
 import jakarta.transaction.Transactional;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
@@ -20,7 +19,6 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -28,48 +26,55 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.NoSuchElementException;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Slf4j
 @Service
-@AllArgsConstructor
 public class BasicBackupIoService implements BackupIoService {
     private final BackupRepository backupRepository;
     private final FileRepository fileRepository;
     private final EmployeeRepository employeeRepository;
-    //추후에 환경설정으로 운영체재별 위지 값 들고 오는 걸로 변경
-    private final String BACKUP_DIR = System.getProperty("os.name").toLowerCase().contains("win")? "C:/myWs/uploadedFiles" : "/Users/mac/myWs/uploadedFilesimg";
-    private final String TIMESTAMP  = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+
+    private final Path rootPath;
+
+    public BasicBackupIoService(BackupRepository backupRepository,
+                                FileRepository fileRepository,
+                                EmployeeRepository employeeRepository,
+                                @Value("${spring.hrbank.storage.local.root-path}") String path
+    ) {
+        this.backupRepository = backupRepository;
+        this.fileRepository = fileRepository;
+        this.employeeRepository = employeeRepository;
+        this.rootPath = Paths.get(System.getProperty("user.dir"), path);
+    }
+
+    private final String TIMESTAMP = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
 
     @Async
     @Override
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    @Transactional
+    //@Transactional
     public void saveBackupData(BackupEvent event) {
 
         //현재 작업 중인 백업 객체 조회
         Backup backup = backupRepository.findById(event.getId())
-                .orElseThrow(()-> new NoSuchElementException("Backup not found"));
+                .orElseThrow(() -> new NoSuchElementException("Backup not found"));
 
         //파일 주소 설정
-        String fileName ="employees_" + TIMESTAMP + ".csv";
-        String filePath = BACKUP_DIR + fileName;
-        Path path = Paths.get(filePath);
+        String fileName = "employees_" + TIMESTAMP + ".csv";
+        Path path = Paths.get(String.valueOf(rootPath), fileName);
 
-        log.info("비동기 CSV 백업 작업을 시작합니다. 저장 위치: {}", filePath);
+        log.info("비동기 CSV 백업 작업을 시작합니다. 저장 위치: {}", path);
 
         //백업 파일 객체 생상 설정
-        File file =new File();
+        File file = new File();
         file.setName(fileName);
-        file.setType("application/json");
-        AtomicInteger fileSize = new AtomicInteger();
-
+        file.setType("text/csv");
 
         try {
-            Files.createDirectories(path.getParent());
+            Files.createDirectories(rootPath);
             try (BufferedWriter writer = Files.newBufferedWriter(path, UTF_8)) {
                 //.csv헤더 설정
                 writer.write("\uFEFF");
@@ -79,42 +84,53 @@ public class BasicBackupIoService implements BackupIoService {
                     //.csv에 한줄식 입력 OOM 방지
                     employeeStreamstream.forEach(employee -> {
                         try {
-                            String csvLine =convertToCsvLine(employee);
-                            fileSize.addAndGet(csvLine.length());
+                            String csvLine = convertToCsvLine(employee);
                             writer.write(csvLine);
                             writer.newLine();
-                        }catch (IOException e){
+                        } catch (IOException e) {
                             throw new RuntimeException("CSV 라인 쓰기 실패", e);
                         }
                     });
                 }
             }
-            log.info("비동기 CSV 백업 작업 성공. {}", filePath);
+            log.info("비동기 CSV 백업 작업 성공. {}", path);
 
-            file.setSize(fileSize.get());
-
-            //파일정보 생성
-            fileRepository.save(file);
+            //파일 사이즈 추가
+            file.setSize((int) Files.size(path));
+            System.out.println();
 
             //백업 상태 성공으로 변경
             backup.setStatus(BackupStatus.COMPLETED);
             backup.setEndedAt(LocalDateTime.now());
             backup.setFile(file);
+
+            //파일정보, 백업정보 저장
+            fileRepository.save(file);
             backupRepository.save(backup);
 
             log.info("비동기 CSV 백업 파일 정보 저장 성공. {}", file);
-        }catch(Exception e) {
+        } catch (Exception e) {
             log.error("비동기 CSV 백업 작업, 백업 파일 정보 저장 중 심각한 오류 발생: {}", e.getMessage());
-            //실패시 미완성 파일 삭제
-            //백업 상태 실패로 변경
             try {
+                //실패시 미완성 파일 삭제
                 Files.deleteIfExists(path);
+
+                //백업 상태 실패로 변경
                 backup.setStatus(BackupStatus.FAILED);
                 backup.setEndedAt(LocalDateTime.now());
+
+                //백업 실패 로그 파일 작성
+                String logFileName = TIMESTAMP+".log";
+                Path logPath = Paths.get(String.valueOf(rootPath), logFileName);
+                Files.writeString(logPath, e.toString(), UTF_8);
+                File logFile = new File(null,logFileName,"text/plain",(int)Files.size(logPath));
+
+                //백업 정보, 에러 로그 파일 정보 저장
+                fileRepository.save(logFile);
                 backupRepository.save(backup);
-                log.info("불완전한 백업 파일을 삭제했습니다: {}", filePath);
+                log.info("불완전한 백업 파일을 삭제했습니다: {}", file);
             } catch (IOException ex) {
-                log.error("불완전한 백업 파일 삭제에 실패했습니다: {}", filePath, ex);
+                log.error("불완전한 백업 파일 삭제에 실패했습니다: {}", file, ex);
             }
         }
     }
