a4dde17d85506d14b1105391857db28509630cf3
jihoon-68 <pkrwlgns64@gmail.com>
2025-10-23 18:59:03 +0900

    fix: 백업 기능 테스트 중에 수정

--- src/main/java/com/sprint/hrbank_sb6_1/mapper/BackupMapper.java
+++ src/main/java/com/sprint/hrbank_sb6_1/mapper/BackupMapper.java
@@ -5,13 +5,9 @@ import com.sprint.hrbank_sb6_1.dto.BackupDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface BackupMapper {
     
-    @Mapping(target = "fileId", expression = "java(backup.getFile().getId())")
+    @Mapping(source = "file.id",target = "fileId")
     BackupDto toBackupDto(Backup backup);
-    
-    List<BackupDto> toBackupDtoList(List<Backup> backups);
 }
--- src/main/java/com/sprint/hrbank_sb6_1/mapper/BackupPagingMapper.java
+++ src/main/java/com/sprint/hrbank_sb6_1/mapper/BackupPagingMapper.java
@@ -1,6 +1,7 @@
 package com.sprint.hrbank_sb6_1.mapper;
 
 import com.sprint.hrbank_sb6_1.dto.BackupDto;
+import com.sprint.hrbank_sb6_1.dto.CursorPageBackupDto;
 import com.sprint.hrbank_sb6_1.dto.CursorPageResponseBackupDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -9,8 +10,9 @@ import org.springframework.data.domain.Slice;
 @Mapper(componentModel = "spring")
 public interface BackupPagingMapper {
 
-    @Mapping(target = "nextCursor", expression = "java(slice.getContent().get(slice.getContent().size()-1).getStartedAt().toString())")
-    @Mapping(target = "nextIdAfter",expression = "java(slice.getContent().get(slice.getContent().size()-1).getId())")
-    @Mapping(source = "totalElements",target = "totalElements")
-    CursorPageResponseBackupDto toCursorPageResponseBackupDto(Slice<BackupDto> slice, Long totalElements);
+    @Mapping(target = "nextCursor", expression = "java(cursorPageBackupDto.getNextCursor())")
+    @Mapping(target = "nextIdAfter",expression = "java(cursorPageBackupDto.getNextIdAfter())")
+    @Mapping(target = "content" ,expression = "java(slice.getContent())")
+    @Mapping(target = "totalElements",expression = "java(cursorPageBackupDto.getTotalElements())")
+    CursorPageResponseBackupDto toCursorPageResponseBackupDto(Slice<BackupDto> slice, CursorPageBackupDto cursorPageBackupDto);
 }
