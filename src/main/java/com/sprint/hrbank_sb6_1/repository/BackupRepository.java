1ca6ec01cef6a22641c45561808aa0e6f06e540f
정진혁 <wlsgur8724@naver.com>
2025-10-24 12:06:44 +0900

    API 진행중

--- src/main/java/com/sprint/hrbank_sb6_1/repository/BackupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.hrbank_sb6_1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface BackupRepository extends JpaRepository<Backup, Long> , BackupRepositoryCustom {
-    Optional<Backup> findTopByStatusOrderByStartedAtDesc(BackupStatus status);
-}
